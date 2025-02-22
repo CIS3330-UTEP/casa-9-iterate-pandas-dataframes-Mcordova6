a5069d80b618ac7bdb169be11d7d1c426c5a8a98
Mcordova6 <mcordova6@miners.utep.edu>
2025-02-22 14:01:16 -0700

    CASA 9 Attempt 1

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,25 @@
+# Open file
 
+filename = "./big-mac-full-index.csv"
+
+# Open pandas
+
+import pandas as pd
+
+df = pd.read_csv(filename)
+
+# Method 4: Using iterrows() method
+
+for index, row in df.iterrows():
+    print(f"{row['date']} {row['name']} Dollar Difference: ${int(row['local_price'] - row['dollar_price'])}")
+
+input()
+    
+# Method 6: Using apply() method
+
+def local_minus_dollar(row):
+    return f"{row['date']} {row['name']} Dollar Difference: ${int(row['local_price'] - row['dollar_price'])}"
+
+result = df.apply(local_minus_dollar, axis=1)
+for res in result:
+    print(res)
